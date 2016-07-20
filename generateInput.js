da29ef51fa50ce05cc857a8b706c4d7523b2e3af
lil33 <elisecorrado@gmail.com>
2016-07-20 16:03:42 -0400

    Add files via upload

---
+++ generateInput.js
@@ -0,0 +1,17 @@
+var generateInput = function() {
+    // Define our port
+    var port = '3000';
+
+    // And the Arduino URL it would correspond to
+    var arduinoURL = 'http://localhost' + ':' + port + '/pin/A0';
+
+    // Go get that URL
+    fetch(arduinoURL).then(function(reading) {
+        // Convert the response we get to text
+        return reading.text();
+    }).then(function(readingText) {
+        // And now re-define `input` (which is defined globally in our HTML file) to that text
+        var number = (parseFloat(readingText)-469)/50;
+        input = number;
+    });
+};
