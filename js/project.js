ac9e4cad2aaba057ecde0fb89228a8dffd73800d
lil33 <elisecorrado@gmail.com>
2016-08-02 23:26:14 -0400

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+        function show(boxNumber)
+        {
+            var div = document.getElementById(boxNumber);
+            div.style.display = 'block';
+        }
+        function hide(boxNumber)
+        {
+            var div = document.getElementById(boxNumber);
+            div.style.display = 'none';
+        }
